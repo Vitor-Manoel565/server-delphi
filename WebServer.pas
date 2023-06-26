6574c4c88ea720ae979b27d4299dbabe0162ee63
Vitor-Manoel02 <vitor@asupernova.com.br>
2023-06-26 18:16:44 -0300

    fix: resolve req

--- WebServer.pas
+++ WebServer.pas
@@ -1,19 +1,19 @@
 {$mode objfpc}{$H+}
 
 uses
-  SysUtils, Classes, fphttpserver;
+  SysUtils, Classes, fphttpserver, fpjson, jsonparser;
 
 type
   TMyHTTPServer = class(TFPHTTPServer)
     procedure RequestHandler(Sender: TObject; var ARequest: TFPHTTPConnectionRequest; var AResponse: TFPHTTPConnectionResponse);
     procedure HandleTesteRequest(AResponse: TFPHTTPConnectionResponse);
     procedure HandleFileRequest(AResponse: TFPHTTPConnectionResponse);
+    procedure HandleWriteRequest(ARequest: TFPHTTPConnectionRequest; AResponse: TFPHTTPConnectionResponse);
   end;
 
 var
   MyHTTPServer: TMyHTTPServer;
-  arquivo: TextFile;
-  texto: string;
+  Data: TJSONData;
 
 procedure TMyHTTPServer.RequestHandler(Sender: TObject; var ARequest: TFPHTTPConnectionRequest; var AResponse: TFPHTTPConnectionResponse);
 begin
@@ -29,35 +29,67 @@ begin
     Exit;
   end;
 
+  if ARequest.URI = '/write' then
+  begin
+    HandleWriteRequest(ARequest, AResponse);
+    Exit;
+  end;
+
   AResponse.Content := 'Hello, world!';
-  AResponse.Code := 200; // HTTP_OK substituído pelo valor inteiro 200
+  AResponse.Code := 200;
   AResponse.SendContent;
 end;
 
 procedure TMyHTTPServer.HandleTesteRequest(AResponse: TFPHTTPConnectionResponse);
 begin
   AResponse.Content := 'Rota "teste" acessada com sucesso!';
-  AResponse.Code := 200; // HTTP_OK substituído pelo valor inteiro 200
+  AResponse.Code := 200;
   AResponse.SendContent;
 end;
 
 procedure TMyHTTPServer.HandleFileRequest(AResponse: TFPHTTPConnectionResponse);
 var
   FileStream: TFileStream;
-  Data: AnsiString;
+  JSONData: TJSONData;
 begin
-  // Use TFileStream para ler o arquivo data.txt
-  FileStream := TFileStream.Create('DB/data.txt', fmOpenRead);
+  FileStream := TFileStream.Create('DB/data.json', fmOpenRead);
   try
-    SetLength(Data, FileStream.Size);
-    FileStream.Read(Data[1], FileStream.Size);
+    JSONData := GetJSON(FileStream);
+    AResponse.Content := JSONData.FormatJSON;
+    AResponse.Code := 200;
+    AResponse.SendContent;
   finally
     FileStream.Free;
   end;
+end;
 
-  AResponse.Content := Data;
-  AResponse.Code := 200; // HTTP_OK substituído pelo valor inteiro 200
-  AResponse.SendContent;
+procedure TMyHTTPServer.HandleWriteRequest(ARequest: TFPHTTPConnectionRequest; AResponse: TFPHTTPConnectionResponse);
+var
+  ReceivedData: String;
+  FileStream: TFileStream;
+begin
+  if ARequest.Method = 'POST' then
+  begin
+    ReceivedData := ARequest.Content;
+
+    Data := GetJSON(ReceivedData);
+    FileStream := TFileStream.Create('DB/data.json', fmCreate);
+    try
+      FileStream.Write(Data.AsJSON[1], Length(Data.AsJSON));
+    finally
+      FileStream.Free;
+    end;
+
+    AResponse.Content := 'Dados JSON escritos com sucesso!';
+    AResponse.Code := 200;
+    AResponse.SendContent;
+  end
+  else
+  begin
+    AResponse.Content := 'Método inválido!';
+    AResponse.Code := 405;
+    AResponse.SendContent;
+  end;
 end;
 
 begin
@@ -67,10 +99,11 @@ begin
     MyHTTPServer.OnRequest := @MyHTTPServer.RequestHandler;
     MyHTTPServer.Active := True;
 
-    writeln('Web server started on http://localhost:8080');
-    writeln('Press [Enter] to quit.');
+    writeln('Servidor web iniciado em http://localhost:8080');
+    writeln('Pressione [Enter] para sair.');
     readln;
   finally
     MyHTTPServer.Free;
+    Data.Free;
   end;
 end.
